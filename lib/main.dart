b51b5a399463fa32e8c1a847152a1d256f6e97ef
Freddy Febres <freddyrfa@gmail.com>
2022-06-19 00:56:31 -0400

    ✨ Stablishing folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_maps/screens/index.dart';
+
 void main() => runApp(const MapsApp());
 
 class MapsApp extends StatelessWidget {
@@ -9,11 +11,7 @@ class MapsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: LoadingScreen(),
       title: 'Maps App',
     );
   }
