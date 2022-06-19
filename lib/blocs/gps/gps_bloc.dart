25f566df8ed70ec6ac46da2af33188617f00915c
Freddy Febres <freddyrfa@gmail.com>
2022-06-19 01:43:08 -0400

    ✨ Installing geolocator on android

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -14,5 +15,30 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
           isGpsEnabled: event.isGpsEnabled,
           isGpsPermissionGranted: event.isGpsPermissionGranted,
         )));
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    final isEnable = await _checkGpsStatus();
+
+    print('isEnable: $isEnable');
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = event.index == 1 ? true : false;
+      print('service status $isEnabled');
+    });
+
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: Limpiar el ServiceStatusStream
+    return super.close();
   }
 }
