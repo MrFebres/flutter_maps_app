a540fb2af6a257a15176dd9d9acab90f7f07e8ad
Freddy Febres <freddyrfa@gmail.com>
2022-06-24 10:31:31 -0400

    ✨ Adding location listeners & dispose

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -1,13 +1,35 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'location_event.dart';
 part 'location_state.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
+  StreamSubscription<Position>? positionStream;
+
   LocationBloc() : super(const LocationState()) {
-    on<LocationEvent>((event, emit) {
-      // TODO: implement event handler
+    on<LocationEvent>((event, emit) {});
+  }
+
+  Future<Position> getCurrentPosition() async =>
+      await Geolocator.getCurrentPosition();
+
+  void startFollowingUser() {
+    positionStream = Geolocator.getPositionStream().listen((event) {
+      print(event);
+      inspect(event);
     });
+  }
+
+  void stopFollowingUser() => positionStream?.cancel();
+
+  @override
+  Future<void> close() {
+    stopFollowingUser();
+    return super.close();
   }
 }
