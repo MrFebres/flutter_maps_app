a2fe028cae60bd245707728600382adc6c91336e
Freddy Febres <freddyrfa@gmail.com>
2022-06-25 18:09:15 -0400

    ✨ Setting MapBloc

---
+++ lib/blocs/map/map_state.dart
@@ -0,0 +1,23 @@
+part of 'map_bloc.dart';
+
+class MapState extends Equatable {
+  const MapState({
+    this.isFollowingUser = false,
+    this.isInitialized = false,
+  });
+
+  final bool isFollowingUser;
+  final bool isInitialized;
+
+  MapState copyWith({
+    bool? isFollowingUser,
+    bool? isInitialized,
+  }) =>
+      MapState(
+        isFollowingUser: isFollowingUser ?? this.isFollowingUser,
+        isInitialized: isInitialized ?? this.isInitialized,
+      );
+
+  @override
+  List<Object> get props => [isFollowingUser, isInitialized];
+}
